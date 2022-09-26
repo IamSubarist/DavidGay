b9a3fb0be0bc97b95e67504ff0536cc3b709d9ad
Roman Petrenko <optimistroma927@gmail.com>
2022-09-26 20:11:49 +0300

    Comm

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+})
